473aa0e6d73b1f76b9e602c83fb7fe1c6718225d
= <=>
2021-09-08 04:29:29 -0300

    Fix spelling.

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import Button from '../Button';
-import Gender from '../Genrer';
+import Gender from '../Gender';
 import Search from '../Search';
 import {
   Container,
